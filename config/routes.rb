93e5c87c9eeefae5b1f6b2337ea5e7bbf6a6694d
GITnvincAdonis <rav30nad0nis@gmail.com>
2026-08-12 15:42:01 -0400

    Use generate devise controllers in route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   resources :homes, only: %i[index]
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "user/sessions",
+    confirmations: "user/confirmations",
+    passwords: "user/passwords",
+    unlocks: "user/unlocks",
+    omniauth_callbacks: "user/omniauth_callbacks",
+    registration_callbacks: "user/registration_callbacks",
+  }, path: "login"
+
   get "up" => "rails/health#show", as: :rails_health_check
   root 'homes#index'
 end
